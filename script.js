ab75cdc7702b9c223d58382e3627524ab9cd8922
Aditya <121721549+adityaSalame@users.noreply.github.com>
2023-06-30 20:36:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+//document.getElementById("btn").addEventListener("click",func);
+function func(){
+	var x=document.getElementById("colorSelect");
+	x.remove(x.selectedIndex);
+}
